a5633446ea6d443a4dad8baca5e39b34a2e0765a
daorozc <daorozc@bancolombia.com.co>
2023-03-31 16:43:49 -0500

    feat: Initialize project

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# azureApiRust
+
+
+Open file:
+```
+code $HOME/.bashrc
+```
+
+Set environmental variables and close the file:
+```
+export AZURE_DEVOPS_PAT=""
+export AZURE_DEVOPS_ORGANIZATION=""
+export AZURE_DEVOPS_PROJECT=""
+export AZURE_DEVOPS_API_VERSION=""
+```
+
+Apply configurations:
+```
+source $HOME/.bashrc
+```
+
+----
+
+Build and execute project:
+```
+cargo run
+```
+
+Only execute project:
+```
+./target/debug/azureapirust
+```
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+// Import required crates and modules
+use base64;
+use reqwest::header::{HeaderValue, AUTHORIZATION};
+use serde_derive::Deserialize;
+use std::env;
+use std::fs;
+
+// Define the main function as asynchronous
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Read the Azure DevOps PAT from the environment variable
+    let pat = env::var("AZURE_DEVOPS_PAT").expect("Set the AZURE_DEVOPS_PAT environment variable");
+    let organization = env::var("AZURE_DEVOPS_ORGANIZATION").expect("Set the AZURE_DEVOPS_ORGANIZATION environment variable");
+    let project = env::var("AZURE_DEVOPS_PROJECT").expect("Set the AZURE_DEVOPS_PROJECT environment variable");
+    let api_version = env::var("AZURE_DEVOPS_API_VERSION").expect("Set the AZURE_DEVOPS_API_VERSION environment variable");
+
+    // Build the base URL for the Azure DevOps API
+    let base_url = format!(
+        "https://{}/{}/_apis/git/repositories?api-version={}",
+        organization, project, api_version
+    );
+
+    // Encode the PAT and create the authorization token
+    let token = format!("Basic {}", base64::encode(format!("PAT:{}", pat)));
+
+    // Create a new reqwest client
+    let client = reqwest::Client::new();
+
+    // Send a GET request to the Azure DevOps API
+    let response = client
+        .get(&base_url)
+        .header(AUTHORIZATION, HeaderValue::from_str(&token)?)
+        .send()
+        .await?;
+
+    // Get the JSON response text
+    let raw_json = response.text().await?;
+
+    // Deserialize the JSON response into AzureReposResponse struct
+    let AzureReposResponse { value, .. }: AzureReposResponse = serde_json::from_str(&raw_json)?;
+
+    // Extract the repository ids and names
+    let repo_info: Vec<String> = value
+        .into_iter()
+        .map(|repo| format!("{} {}", repo.id, repo.name))
+        .collect();
+
+    // Define the output file name
+    let output_file = "repo_info.txt";
+
+    // Write the repository names to the output file, separated by newlines
+    fs::write(output_file, repo_info.join("\n"))?;
+
+    // Print the output file name
+    println!("Repository info have been saved to: {}", output_file);
+
+    // Return Ok result
+    Ok(())
+}
+
+// Define a struct for deserializing the Azure DevOps API response
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct AzureReposResponse {
+    count: usize,
+    value: Vec<AzureRepo>,
+}
+
+// Define a struct for deserializing the individual Azure repositories
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct AzureRepo {
+    id: String,
+    name: String,
+}
